1a045d70c012409f5cc8b352da2ab0f93ccaa6eb
Giresse Ashu-Bob <ashubobg@yahoo.co.uk>
2018-03-01 22:23:45 +0200

    added delete and show routes

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -11,8 +11,10 @@ class PostsIndex extends Component {
     renderPosts() {
        return  _.map(this.props.posts, post => {
            return (
-               <li className="list-group-item" key={post.id}>
-                   {post.title}
+               <li key={post.id}>
+                   <Link to={`/posts/${post.id}`}>
+                       {post.title}
+                   </Link>
                </li>
            );
        });
@@ -21,9 +23,9 @@ class PostsIndex extends Component {
         return (
             <div>
                  <div className="text-xs-right">
-                        <Link className="btn btn-primary" to="/posts/new">
+                     <Link className="btn btn-primary" to="/posts/new">
                             Add a Post
-                        </Link>
+                    </Link>
                  </div>
                 <h3>Posts</h3>
                 <ul className="list-group">
--- src/index.js
+++ src/index.js
@@ -18,9 +18,9 @@ ReactDOM.render(
       <BrowserRouter>
           <div>
             <Switch>
-            <Route path="/posts/new" component={PostsNew} />
-            <Route path="/posts/:id" component={PostsShow} />
-            <Route path="/" component={PostsIndex} />
+                <Route path="/posts/new" component={PostsNew} />
+                <Route path="/posts/:id" component={PostsShow} />
+                <Route path="/" component={PostsIndex} />
             </Switch>
           </div>
       </BrowserRouter>
